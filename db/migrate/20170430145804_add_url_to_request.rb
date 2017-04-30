9b4dafba95fe647c64210afab8b4e49362fe73d7
Derek Edwards <dedwards8@gmail.com>
2017-04-30 12:00:56 -0400

    Add basic view for Request

--- db/migrate/20170430145804_add_url_to_request.rb
+++
@@ -1,6 +0,0 @@
-class AddUrlToRequest < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :requests, :url, :string, default: false
-  	add_column :requests, :name, :string, default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430145804) do
+ActiveRecord::Schema.define(version: 20170430152352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,16 @@ ActiveRecord::Schema.define(version: 20170430145804) do
     t.string   "name",       default: "f"
   end
 
+  create_table "tests", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name",       null: false
+    t.boolean  "status"
+    t.text     "key"
+    t.text     "value"
+    t.integer  "request_id"
+    t.index ["request_id"], name: "index_tests_on_request_id", using: :btree
+  end
+
+  add_foreign_key "tests", "requests"
 end
