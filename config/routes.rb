852fc28fbcc394196edb46721004fe2e9130c229
Derek Edwards <dedwards8@gmail.com>
2017-04-30 16:46:20 -0400

    Add links from requests to tests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :requests, :only => [:index]
+  resources :requests, :only => [:index, :show]
+
 end
