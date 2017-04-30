458be647bc37c47ead7e191bcdc198fcb09db24c
Derek Edwards <dedwards8@gmail.com>
2017-04-30 17:32:46 -0400

    Run URL

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,29 @@
 class Request < ApplicationRecord
+  
+  require 'net/http'
+
   has_many :tests
+
+  attr_reader :response_code
+  attr_reader :response_body
 
   def status
   	(tests.failing.count > 0) ? false : true
+  end
+
+  def update
+   	@response_code, @response_body = self.call
+   	tests.each do |test|
+   	  test.run
+    end
+  end
+
+  def call 
+    uri = URI.parse(url)
+    req = Net::HTTP::Get.new(uri.to_s)
+    res = Net::HTTP.start(uri.host, uri.port) {|http|
+      http.request(req)
+    }
+    return res.code, res.body
   end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,15 @@
 class Test < ApplicationRecord
 
-	belongs_to :request
+  belongs_to :request
 
-	scope :failing, -> { where(:status => false) }
-    scope :passing, -> { where(:status => true) }
+  scope :failing, -> { where(:status => false) }
+  scope :passing, -> { where(:status => true) }
+
+  def run
+    self.status = true
+    self.save
+
+    puts name.ai 
+  end
 
 end
