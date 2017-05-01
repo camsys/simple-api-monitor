d70a642ecad6bdcc8a0f9116260fe62dd30f983d
Derek Edwards <dedwards8@gmail.com>
2017-04-30 20:41:50 -0400

    Add devise

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module SimpleApiMonitor
   class Application < Rails::Application
-
   	config.time_zone = 'Eastern Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
